c956655d8a0475e2e8d6b7e3b8a659e9b46698cc
Sriharsha Gundappa <sriharsha.gundappa@dailymotion.com>
2018-11-15 14:07:04 -0500

    Adding function to displayAd

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"sync"
+    "math/rand"
+	"fmt"
 )
 
 var (
@@ -20,6 +22,7 @@ func init() {
 	http.Handle("/", http.FileServer(http.Dir("")))
 	http.HandleFunc("/event", storeEvent)
 	http.HandleFunc("/events", listEvents)
+    http.HandleFunc("/displayAd", displayAd)
 }
 
 func main() {
@@ -51,6 +54,41 @@ func listEvents(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+func displayAd(w http.ResponseWriter, r *http.Request) {
+    userXids, ok := r.URL.Query()["userXid"]
+    
+    if !ok || len(userXids[0]) < 1 {
+        log.Println("Url Param 'userXid' is missing")
+        return
+    }
+
+    userXid := userXids[0]
+
+    log.Println("UserXid ", userXid);
+    
+    videoXids, ok := r.URL.Query()["videoXid"]
+    
+    if !ok || len(videoXids[0]) < 1 {
+        log.Println("Url Param 'videoXids' is missing")
+        return
+    }
+    
+    videoXid := videoXids[0]
+
+	log.Println("videoXid ", videoXid);
+    
+    w.WriteHeader(http.StatusOK);
+
+    randomNum := rand.Intn(10)
+    
+    if randomNum >= 8 {
+        fmt.Fprintf(w, "false")
+    } else {
+        fmt.Fprintf(w, "true")
+    }
+
 }
 
 type User struct {
