fb901fd2175cdbb82d7de4db6d454d6f7a874145
Sriharsha Gundappa <sriharsha.gundappa@dailymotion.com>
2018-11-15 21:31:22 -0500

    database changes

--- main.go
+++ main.go
@@ -6,18 +6,29 @@ import (
 	"log"
 	"net/http"
 	"sync"
-    "math/rand"
+  //  "math/rand"
 	"fmt"
+	"database/sql"
+	_ "github.com/lib/pq"
 )
 
 var (
 	addr       string
 	events     []Event
 	eventsLock sync.Mutex
+	postgre		*PostgreClient
 )
+
+const UPDATE_VIDEO_WATCHED = "update dm_user set videos_watched = videos_watched + 1, tokens_to_redeem = tokens_to_redeem+1 where user_id = $1"
+const UPDATE_AD_WATCHED = "update dm_user set ads_watched = ads_watched + 1, tokens_to_redeem = tokens_to_redeem+1 where user_id = $1"
+const SELECT_REDEEM_COUNT = "select reduce_ads, tokens_to_redeem from dm_user where user_id = $1"
+const UPDATE_REDEEM_COUNT = "update dm_user set tokens_to_redeem = tokens_to_redeem - 4 where user_id = $1"
+
 
 func init() {
 	flag.StringVar(&addr, "addr", ":8080", "listen addr for the HTTP server")
+
+	postgre = NewPostgreClient()
 
 	http.Handle("/", http.FileServer(http.Dir("")))
 	http.HandleFunc("/event", storeEvent)
@@ -39,11 +50,27 @@ func storeEvent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	eventsLock.Lock()
-	events = append(events, event)
-	eventsLock.Unlock()
+	if (event.Type == "video_end") || (event.Type == "ad_end") && (event.User.DailymotionID != "")  {
+		db := postgre.getDbConnection()
+		defer db.Close()
+
+		query := UPDATE_VIDEO_WATCHED
+
+		if event.Type == "ad_end" {
+			query = UPDATE_AD_WATCHED
+		}
+
+		db.QueryRow(query, event.User.DailymotionID)
+
+	}
 
 	w.WriteHeader(http.StatusCreated)
+
+	/* eventsLock.Lock()
+	events = append(events, event)
+	eventsLock.Unlock() */
+
+
 }
 
 func listEvents(w http.ResponseWriter, r *http.Request) {
@@ -78,16 +105,35 @@ func displayAd(w http.ResponseWriter, r *http.Request) {
     videoXid := videoXids[0]
 
 	log.Println("videoXid ", videoXid);
-    
-    w.WriteHeader(http.StatusOK);
 
-    randomNum := rand.Intn(10)
+	db := postgre.getDbConnection()
+	defer db.Close()
+
+	rows, err := db.Query(SELECT_REDEEM_COUNT, userXid)
+	checkErr(err)
+
+	for rows.Next() {
+		var reduce_ads bool
+		var tokens_to_redeem int
+		err = rows.Scan(&reduce_ads, &tokens_to_redeem)
+		checkErr(err)
+		if (reduce_ads == true) && (tokens_to_redeem > 4) {
+			db.QueryRow(UPDATE_REDEEM_COUNT, userXid)
+			fmt.Fprintf(w, "true")
+		} else {
+			fmt.Fprintf(w, "false")
+		}
+	}
+
+	w.WriteHeader(http.StatusOK);
+
+    /* randomNum := rand.Intn(10)
     
     if randomNum >= 8 {
         fmt.Fprintf(w, "false")
     } else {
         fmt.Fprintf(w, "true")
-    }
+    } */
 
 }
 
@@ -99,4 +145,27 @@ type Event struct {
 	User    User   `json:"user"`
 	Type    string `json:"type"`
 	VideoID string `json:"video_id"`
+}
+
+type PostgreClient struct {
+
+}
+
+func NewPostgreClient() *PostgreClient {
+	return &PostgreClient{}
+}
+
+
+func (pc *PostgreClient) getDbConnection() *sql.DB  {
+	dbinfo := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
+		"hourlymotion.cgwqhqmxi2gt.us-east-1.rds.amazonaws.com", "hourlyadmin", "DMhackathon2018", "hourlymotiondb")
+	db, err := sql.Open("postgres", dbinfo)
+	checkErr(err)
+	return db
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
