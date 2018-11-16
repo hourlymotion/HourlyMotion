a3ea365653e4bc093938f038dfd7b97234572040
Vincent <hourlymotion@gmail.com>
2018-11-16 13:03:09 +0100

    admin and user dashboards

--- main.go
+++ main.go
@@ -24,6 +24,9 @@ func init() {
 	http.Handle("/", http.FileServer(http.Dir("")))
 	http.HandleFunc("/event", storeEvent)
 	http.HandleFunc("/displayAd", displayAd)
+	http.HandleFunc("/admin-data", adminData)
+	http.HandleFunc("/user-data", userData)
+	http.HandleFunc("/user-settings", userSettings)
 }
 
 func main() {
@@ -65,7 +68,7 @@ func storeEvent(w http.ResponseWriter, r *http.Request) {
 		var userData UserData
 		bucket := tx.Bucket(k("hourlymotion"))
 		data := bucket.Get(k(event.User.DailymotionID))
-		if data == nil {
+		if len(data) == 0 {
 			userData = UserData{
 				Xid: event.User.DailymotionID,
 			}
@@ -123,7 +126,7 @@ func shouldDisplayAd(userXid string) (bool, error) {
 		var userData UserData
 		bucket := tx.Bucket(k("hourlymotion"))
 		data := bucket.Get(k(userXid))
-		if data == nil {
+		if len(data) == 0 {
 			userData = UserData{
 				Xid: userXid,
 			}
@@ -142,6 +145,7 @@ func shouldDisplayAd(userXid string) (bool, error) {
 		if !displayAd {
 			log.Printf("will NOT display an ad for user %#v", userData)
 			userData.Tokens--
+			userData.UsedTokens++
 		}
 
 		log.Printf("(maybe) updated user data %#v", userData)
@@ -154,11 +158,117 @@ func shouldDisplayAd(userXid string) (bool, error) {
 	return displayAd, err
 }
 
+func adminData(w http.ResponseWriter, r *http.Request) {
+	usersData := []UserData{}
+	err := db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(k("hourlymotion"))
+		return bucket.ForEach(func(k, v []byte) error {
+			var userData UserData
+			if err := json.Unmarshal(v, &userData); err != nil {
+				return err
+			}
+			usersData = append(usersData, userData)
+			return nil
+		})
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err = json.NewEncoder(w).Encode(usersData); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func userData(w http.ResponseWriter, r *http.Request) {
+	userXid := r.URL.Query().Get("userXid")
+	if len(userXid) == 0 {
+		http.Error(w, "missing userXid query parameter", http.StatusBadRequest)
+		return
+	}
+
+	var userData *UserData
+	err := db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(k("hourlymotion"))
+		data := bucket.Get(k(userXid))
+		if len(data) > 0 {
+			userData = new(UserData)
+			if err := json.Unmarshal(data, userData); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if userData == nil {
+		http.NotFound(w, r)
+		return
+	}
+	if err = json.NewEncoder(w).Encode(userData); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func userSettings(w http.ResponseWriter, r *http.Request) {
+	userXid := r.URL.Query().Get("userXid")
+	if len(userXid) == 0 {
+		http.Error(w, "missing userXid query parameter", http.StatusBadRequest)
+		return
+	}
+
+	var (
+		settings Settings
+		err      error
+	)
+	if err = json.NewDecoder(r.Body).Decode(&settings); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		var userData UserData
+		bucket := tx.Bucket(k("hourlymotion"))
+		data := bucket.Get(k(userXid))
+		if len(data) == 0 {
+			userData = UserData{
+				Xid: userXid,
+			}
+		} else {
+			if err := json.Unmarshal(data, &userData); err != nil {
+				return err
+			}
+		}
+		userData.Settings = settings
+		log.Printf("Storing/Updating settings %#v for user %s", settings, userXid)
+		data, err := json.Marshal(userData)
+		if err != nil {
+			return err
+		}
+		return bucket.Put(k(userXid), data)
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
 type UserData struct {
-	Xid    string
-	Ads    int64
-	Videos int64
-	Tokens int64
+	Xid        string
+	Ads        int64
+	Videos     int64
+	Tokens     int64
+	UsedTokens int64
+	Settings   Settings
+}
+
+type Settings struct {
 }
 
 type User struct {
